1f1a6a0df91feed4b0494f45d9841af20bd7e107
James Holden <jholden@ucsd.edu>
2018-10-08 12:20:07 -0700

    working 1-factorial

--- 1-factorial/factorial.rs
+++ 1-factorial/factorial.rs
@@ -1,7 +1,34 @@
 use std::io;
+use std::process;
 
+// Write a programme which finds the factorial of a 
+// number entered by the user. (check for all conditions).
 fn main() {
-    println!("Enter the number whose factorial you want to calculate.");
+    let mut input = String::new();
+    let mut count = 0u64;
+    let mut total = 1u64;
 
-    let mut n = "n";
+    println!("Enter the an integer [1, 12] whose factorial you want to calculate.");
+        
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line.");
+    
+    let n: u64 =  input.trim().parse()
+        .expect("N must be a number");
+   
+    if n <= 0 || n >= 13 {
+        println!("Please enter a integer within subset [1, 12]. Exiting...");
+        process::exit(1);
+    }
+
+    loop {
+        total = total * (n - count);    
+        count += 1;
+        if count >= n-1 {
+            break;
+        }
+    }
+
+    println!("The result of {}! is {}.", n, total);
+
 }
